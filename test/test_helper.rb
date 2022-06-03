07d9dfcea7c52d4e1ed43eaa8da7642cea7a8e01
PJ <peter.wdj@gmail.com>
2022-06-03 12:03:57 +0100

    Adds test setup to be able to use database tables in our tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,47 @@
 # frozen_string_literal: true
+require "rubygems"
+require "bundler/setup"
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+
 require "taylors_enum"
+require "active_record"
+require "active_support"
 
 require "minitest/autorun"
+
+db_config = YAML.load_file(File.expand_path("../database.yml", __FILE__)).fetch("postgresql")
+ActiveRecord::Base.establish_connection(db_config)
+
+def setup_db
+  ActiveRecord::Base.connection.create_table :albums do |t|
+    t.column :name, :string
+    t.column :created_at, :datetime
+    t.column :updated_at, :datetime
+  end
+end
+
+def teardown_db
+  tables =
+    if ActiveRecord::VERSION::MAJOR >= 5
+      ActiveRecord::Base.connection.data_sources
+    else
+      ActiveRecord::Base.connection.tables
+    end
+
+  tables.each do |table|
+    ActiveRecord::Base.connection.drop_table(table)
+  end
+end
+
+class Album < ActiveRecord::Base
+  self.table_name = 'albums'
+  taylors_enum name: %w[debut fearless speak_now red nineteen_eighty_nine reputation lover folklore evermore]
+end
--- test/test_taylors_enum.rb
+++ test/test_taylors_enum.rb
@@ -3,6 +3,14 @@
 require "test_helper"
 
 class TestTaylorsEnum < Minitest::Test
+  def setup
+    setup_db
+  end
+
+  def teardown
+    teardown_db
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::TaylorsEnum::VERSION
   end
